cd3ba6de34ec262ba6c99302ac042c5e86abd756
Eduard-Stefan <cojocaru.eduard.p6b@student.ucv.ro>
2023-07-09 14:22:07 +0300

    added sort and filter for player

--- src/main/java/com/example/championship/controller/PlayerController.java
+++ src/main/java/com/example/championship/controller/PlayerController.java
@@ -5,7 +5,9 @@ import com.example.championship.service.PlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/player")
@@ -14,8 +16,46 @@ public class PlayerController {
     private PlayerService playerService;
 
     @GetMapping(value = "/all")
-    public List<Player> getAllPlayers() {
-        return playerService.getAllPlayers();
+    public List<Player> getAllPlayers(
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(required = false) String sortOrder,
+            @RequestParam(required = false) String filterValue) {
+
+        List<Player> players = playerService.getAllPlayers();
+
+        if (sortBy != null && sortOrder != null) {
+            Comparator<Player> comparator = null;
+
+            switch (sortBy.toLowerCase()) {
+                case "name" -> comparator = Comparator.comparing(Player::getName, String.CASE_INSENSITIVE_ORDER);
+                case "age" -> comparator = Comparator.comparingInt(Player::getAge);
+                case "nationality" ->
+                        comparator = Comparator.comparing(Player::getNationality, String.CASE_INSENSITIVE_ORDER);
+                case "team" ->
+                        comparator = Comparator.comparing(p -> p.getTeam().getName(), String.CASE_INSENSITIVE_ORDER);
+                default -> {
+                }
+            }
+
+            if (comparator != null) {
+                if (sortOrder.equalsIgnoreCase("desc")) {
+                    comparator = comparator.reversed();
+                }
+                players.sort(comparator);
+            }
+        }
+
+        if (filterValue != null) {
+            String filterValueLowerCase = filterValue.toLowerCase();
+            players = players.stream()
+                    .filter(player ->
+                            player.getName().toLowerCase().contains(filterValueLowerCase)
+                                    || player.getNationality().toLowerCase().contains(filterValueLowerCase)
+                                    || (player.getTeam() != null && player.getTeam().getName().toLowerCase().contains(filterValueLowerCase)))
+                    .collect(Collectors.toList());
+        }
+
+        return players;
     }
 
     @PostMapping(value = "/new")
@@ -43,5 +83,4 @@ public class PlayerController {
                     return playerService.save(newPlayer);
                 });
     }
-
 }
