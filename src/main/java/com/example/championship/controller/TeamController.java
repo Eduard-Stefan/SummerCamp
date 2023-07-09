d2b74b216b11258714da387d1df7d36e63057774
Eduard-Stefan <cojocaru.eduard.p6b@student.ucv.ro>
2023-07-09 14:36:45 +0300

    added sort and filter for team

--- src/main/java/com/example/championship/controller/TeamController.java
+++ src/main/java/com/example/championship/controller/TeamController.java
@@ -5,7 +5,9 @@ import com.example.championship.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/team")
@@ -14,8 +16,42 @@ public class TeamController {
     private TeamService teamService;
 
     @GetMapping(value = "/all")
-    public List<Team> getAllTeams() {
-        return teamService.getAllTeams();
+    public List<Team> getAllTeams(
+            @RequestParam(required = false) String sortBy,
+            @RequestParam(required = false) String sortOrder,
+            @RequestParam(required = false) String filterValue) {
+
+        List<Team> teams = teamService.getAllTeams();
+
+        if (sortBy != null && sortOrder != null) {
+            Comparator<Team> comparator = null;
+
+            switch (sortBy.toLowerCase()) {
+                case "name" -> comparator = Comparator.comparing(Team::getName, String.CASE_INSENSITIVE_ORDER);
+                case "location" -> comparator = Comparator.comparing(Team::getLocation, String.CASE_INSENSITIVE_ORDER);
+                case "coach" -> comparator = Comparator.comparing(Team::getCoach, String.CASE_INSENSITIVE_ORDER);
+                default -> {
+                }
+            }
+
+            if (comparator != null) {
+                if (sortOrder.equalsIgnoreCase("desc")) {
+                    comparator = comparator.reversed();
+                }
+                teams.sort(comparator);
+            }
+        }
+
+        if (filterValue != null) {
+            String filterValueLowerCase = filterValue.toLowerCase();
+            teams = teams.stream()
+                    .filter(team -> team.getName().toLowerCase().contains(filterValueLowerCase)
+                            || team.getLocation().toLowerCase().contains(filterValueLowerCase)
+                            || team.getCoach().toLowerCase().contains(filterValueLowerCase))
+                    .collect(Collectors.toList());
+        }
+
+        return teams;
     }
 
     @PostMapping(value = "/new")
@@ -45,5 +81,4 @@ public class TeamController {
                     return teamService.save(newTeam);
                 });
     }
-
 }
